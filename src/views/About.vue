2403cd643fa0b98dfc6489299269d54fcab5f3d3
andreyromanov <andreygorogogo@gmail.com>
2020-02-04 23:20:56 +0200

    Initial commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,45 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div id="app">
+  <h1>Bitcoin Price Index</h1>
+  <div
+    v-for="currency in info"
+    class="currency"
+    v-bind:key="currency"
+  >
+    {{ currency.description }}:
+    <span class="lighten">
+      <span v-html="currency.symbol"></span>{{ currency.rate_float | currencydecimal }}
+    </span>
+  </div>
+</div>
   </div>
 </template>
+
+<script>
+
+import axios from 'axios';
+
+export default{
+  data() {
+    return {
+      info: null,
+      loading: true,
+      errored: false
+    };
+  },
+  filters: {
+    currencydecimal(value) {
+      return value.toFixed(2);
+    }
+  },
+  mounted() {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => {
+        this.info = response.data.bpi;
+      });
+  }
+};
+</script>
