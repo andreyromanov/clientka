254aab9783da0329e4b4aad51c86109f950d808b
andreyromanov <andreygorogogo@gmail.com>
2020-02-08 10:59:36 +0200

    Seo&categories  sections

--- src/components/Popular.vue
+++ src/components/Popular.vue
@@ -1,19 +1,35 @@
 <template>
-	<div class="border p-5">
-		<div
-		    v-for="category in info"
-		    class="category"
-		    v-bind:key="category.id"
-		  >
-    {{ category.id }}:{{ category.name }}
-  </div>
+	<div style="background-color: lightblue">
+	<div class="container border-top py-md-5">
+		<h3 class="mb-5">Популярные категории</h3>
+  <vueper-slides
+  class="no-shadow"
+  :visible-slides="4"
+  slide-multiple
+  :gap="3"
+  :slide-ratio="1 / 4"
+  :dragging-distance="200"
+  :breakpoints="{ 800: { visibleSlides: 2, slideMultiple: 2 } }">
+  <vueper-slide class="border" v-for="category in info" :key="category.id" :title="category.name"/>
+</vueper-slides>
+	<router-link to="/about">
+		<button class="btn btn-warning btn-lg mt-5">
+			Все категории
+		</button>
+	</router-link>
 	</div>
+</div>
 </template>
 <script>
 
 import axios from 'axios';
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
 
 export default{
+  components: { 
+  	VueperSlides, VueperSlide 
+  },
   data() {
     return {
       info: null
@@ -28,3 +44,8 @@ export default{
   }
 };
 </script>
+<style type="text/css">
+	.vueperslides__bullets{
+		display: none;
+	}
+</style>
--- src/components/SeoText.vue
+++ src/components/SeoText.vue
@@ -1,5 +1,41 @@
 <template>
-	<div>
-		SEO
+	<div class="container">
+	<div class="p-md-5 text-left">
+		<h4 class="mb-4 font-weight-bold">Интернет магазин для всех</h4>
+
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+		<h5 class="my-3 font-weight-bold">dfihdsfsdhfs</h5>
+
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</div>
 	</div>
 </template>
