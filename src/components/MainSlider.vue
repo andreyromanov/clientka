42604811504199af0dded816074bcd8316dc6db4
andreyromanov <andreygorogogo@gmail.com>
2020-02-08 18:53:06 +0200

    Added images to slider

--- src/components/MainSlider.vue
+++ src/components/MainSlider.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
 		<h1>Main Slider</h1>
-		<img alt="Vue logo" src="../assets/logo.png" />
+		<img style="width: 100%" alt="Vue logo" src="https://firebasestorage.googleapis.com/v0/b/vue-shop-2d38f.appspot.com/o/productsrocket.png?alt=media&token=eb31e37a-1994-42cb-8c49-39c7295bd2fc" />
 	</div>
 </template>
--- src/components/Popular.vue
+++ src/components/Popular.vue
@@ -10,13 +10,26 @@
   :slide-ratio="1 / 4"
   :dragging-distance="200"
   :breakpoints="{ 800: { visibleSlides: 2, slideMultiple: 2 } }">
-  <vueper-slide class="border" v-for="category in info" :key="category.id" :title="category.name"/>
+  <vueper-slide class="border" v-for="category in info" :key="category.id" >
+    <template v-slot:content>
+      <router-link to="/about">
+      <div>
+        <div style="height: 230px">
+          <img :src="category.image" style="height: 100%;width: 100%">
+        </div>
+        <div style="background-color: #fff;padding: 10px 0;" class="border">{{ category.name }}</div>
+      </div>
+      </router-link>
+    </template>
+  </vueper-slide>
 </vueper-slides>
+
 	<router-link to="/about">
 		<button class="btn btn-warning btn-lg mt-5">
 			Все категории
 		</button>
 	</router-link>
+
 	</div>
 </div>
 </template>
