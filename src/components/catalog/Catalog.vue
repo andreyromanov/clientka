674ed20ab3e15d3158b1c52b7c0cfae1cd27aada
andreyromanov <andreygorogogo@gmail.com>
2020-03-16 21:08:17 +0200

    fixed catalog cards

--- src/components/catalog/Catalog.vue
+++ src/components/catalog/Catalog.vue
@@ -2,25 +2,28 @@
 	<div>
 		<div class="container my-5">
 			<div class="row">
-				<div class="col-md-3  my-2" v-for="product in info" :key="product.id" >
-          <div class="border box-shadow px-2 catalog-item">
-					<div style="height: 200px;" class="">
-			       <img :src="product.image"  class="image-item">     
-			    </div>
+				<div class="col-md-3 my-2" v-for="product in info" :key="product.id" >
+          <h5>
+            {{product.name}}
+          </h5>
+          <div class="border box-shadow p-2 catalog-item">
+  					<div class="item-image-div">
+  			       <img :src="product.image"  class="image-item">     
+  			    </div>
 
-	         <div class="row pt-3">
-                <div class="col-md-5 pt-2">
-                   <b style="font-size: 22px;">{{product.price}} $</b>
-                </div>
-                <div class="col-md-7 text-right">
-                  <button @click="addToFav(product.id)" class="btn btn-primary">
-                    <i class="fa fa-heart fa-2x" aria-hidden="true"></i>
-                  </button>
-                  <button @click="addToBasket(product.id)" class="btn btn-success ml-2">
-                    <i class="fa fa-plus fa-2x" aria-hidden="true"></i>
-                  </button>
-                </div>
-		       </div>
+  	         <div class="row pt-3">
+                  <div class="col-md-5 pt-2">
+                     <b class="h4">{{product.price}} $</b>
+                  </div>
+                  <div class="col-md-7 text-right">
+                    <button @click="addToFav(product.id)" class="btn btn-primary">
+                      <i class="fa fa-heart fa-2x" aria-hidden="true"></i>
+                    </button>
+                    <button @click="addToBasket(product.id)" class="btn btn-success ml-2">
+                      <i class="fa fa-plus fa-2x" aria-hidden="true"></i>
+                    </button>
+                  </div>
+  		       </div>
            </div>
 				</div>
 			</div>
